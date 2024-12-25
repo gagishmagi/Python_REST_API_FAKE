be6111b1dfc466e4248871f9db49f24212d8a888
gagishmagi <gagi.shmagi@gmail.com>
2024-12-25 11:43:58 +0200

    Updated code to generate all resources

--- src/create_db.py
+++ src/create_db.py
@@ -3,11 +3,14 @@ from faker import Faker
 
 fake = Faker()
 
-def generate_fake_data(num_users=10, num_posts=100, num_comments=500):
+def generate_fake_data(num_users=10, num_posts=100, num_comments=500, num_albums=100, num_photos=500, num_todos=200):
     data = {
         "users": [],
         "posts": [],
-        "comments": []
+        "comments": [],
+        "albums": [],
+        "photos": [],
+        "todos": []
     }
 
     for i in range(1, num_users + 1):
@@ -54,6 +57,33 @@ def generate_fake_data(num_users=10, num_posts=100, num_comments=500):
             "body": fake.paragraph(nb_sentences=2)
         }
         data["comments"].append(comment)
+
+    for i in range(1, num_albums + 1):
+        album = {
+            "id": i,
+            "userId": fake.random_element(elements=data["users"])["id"],
+            "title": fake.sentence(nb_words=3)
+        }
+        data["albums"].append(album)
+
+    for i in range(1, num_photos + 1):
+        photo = {
+            "id": i,
+            "albumId": fake.random_element(elements=data["albums"])["id"],
+            "title": fake.sentence(nb_words=3),
+            "url": fake.image_url(),
+            "thumbnailUrl": fake.image_url()
+        }
+        data["photos"].append(photo)
+
+    for i in range(1, num_todos + 1):
+        todo = {
+            "id": i,
+            "userId": fake.random_element(elements=data["users"])["id"],
+            "title": fake.sentence(nb_words=3),
+            "completed": fake.boolean()
+        }
+        data["todos"].append(todo)
 
     return data
 
